11c285d36f52a9731cd9ea377a6c0c8481dcfb2f
evanpurl <evan@purlcomputers.com>
2023-03-05 12:18:00 -0700

    added webpage

--- cogs/webserver.py
+++ cogs/webserver.py
@@ -17,14 +17,12 @@ class ServerCog(commands.Cog):
         await self.bot.wait_until_ready()
         await self.server.start()
 
-    routes = web.RouteTableDef()
-
     @server.add_route(path="/", method="GET", cog="ServerCog")
-    async def home(self):
+    async def home(self, request):
         return web.Response(
             text=f'<h1>{self.bot.user.name} is online!</h1> \n <img src="{self.bot.user.avatar.url}" alt="Bot avatar" '
                  f'style="display: block;margin-left: auto;margin-right: auto;width: 50%;" width="125" height="125">',
-            content_type='text/html')
+            content_type='text/html', status=200)
 
 
 async def setup(bot):
