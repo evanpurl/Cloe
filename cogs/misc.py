3abc1bba9ce4f183f064059803564c006d0dac96
evan <evan@purlcomputers.com>
2023-04-28 12:55:38 -0600

    removed botnet functions

--- cogs/misc.py
+++ cogs/misc.py
@@ -2,9 +2,6 @@ import discord
 from discord import app_commands
 from discord.ext import commands
 
-from util.accessutils import whohasaccess
-from util.dbsetget import dbsetbotnetwork
-
 
 class misccommands(commands.Cog):
 
@@ -20,17 +17,6 @@ class misccommands(commands.Cog):
             **Auto adding people to the 'Player' role.**
             **I'll add to this later.**""",
             ephemeral=True)
-
-    @app_commands.command(name="addbot", description="Command for Purls to add bots to the bot network.")
-    async def addbot(self, interaction: discord.Interaction, bot: discord.User, connection: str, owner: discord.User):
-        try:
-            if await whohasaccess(interaction.user.id):
-                await dbsetbotnetwork(bot.name, bot.id, connection, owner.id)
-                await interaction.response.send_message(content=f"{bot.name} with connection address {connection} and owner {owner.name} has been added to the bot network.", ephemeral=True)
-            else:
-                await interaction.response.send_message(content=f"You can't run this command.", ephemeral=True)
-        except Exception as e:
-            await interaction.response.send_message(content=f"Something went wrong", ephemeral=True)
 
 
 async def setup(bot):
