b79d83705dd1e67f6c454b3c0f59277a244e93f7
evanpurl <evan@purlcomputers.com>
2023-04-30 17:36:55 -0600

    added invite link to /version

--- cogs/misc.py
+++ cogs/misc.py
@@ -11,7 +11,8 @@ class misccommands(commands.Cog):
     @app_commands.command(name="version", description="Slash command for Cloe's version.")
     async def version(self, interaction: discord.Interaction):
         await interaction.response.send_message(
-            content=f"""{interaction.user.mention}, this is the 2.1.5 version of Cloe. You can find my list of features here: https://bots.botsbypurls.com/C1o3.html""",
+            content=f"""{interaction.user.mention}, this is the 2.1.5 version of Cloe. You can find my list of features here: https://bots.botsbypurls.com/C1o3.html
+                    Discord: https://discord.gg/g8UbZ95QZh""",
             ephemeral=True)
 
 
