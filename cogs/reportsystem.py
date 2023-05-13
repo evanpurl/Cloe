c2625286f10172a9283268a495750698ef799c49
evanpurl <evan@purlcomputers.com>
2023-05-12 20:21:29 -0600

    fixed bug with report category

--- cogs/reportsystem.py
+++ cogs/reportsystem.py
@@ -4,6 +4,8 @@ import discord
 from discord import app_commands, ui
 from discord.ext import commands
 
+from util.dbsetget import dbset, dbget
+
 timeout = 300  # seconds
 
 
@@ -63,6 +65,7 @@ class reportbutton(discord.ui.View):
                        custom_id="reportbutton")
     async def gray_button(self, interaction: discord.Interaction, button: discord.ui.Button):
         try:
+            cat = await dbget(serverid=interaction.guild.id, bot=interaction.client.user.name, valuetoget="categoryid")
             existticket = discord.utils.get(interaction.guild.channels,
                                             name=f"report-{interaction.user.name.lower()}{interaction.user.discriminator}")
             if existticket:
@@ -74,7 +77,7 @@ class reportbutton(discord.ui.View):
                     interaction.guild.default_role: discord.PermissionOverwrite(read_messages=False),
                     interaction.user: discord.PermissionOverwrite(read_messages=True),
                     interaction.guild.me: discord.PermissionOverwrite(read_messages=True)}
-                ticketcat = discord.utils.get(interaction.guild.categories, name="REPORTS")
+                ticketcat = discord.utils.get(interaction.guild.categories, id=cat[0])
                 if ticketcat:
                     ticketchan = await interaction.guild.create_text_channel(
                         f"report-{interaction.user.name}{interaction.user.discriminator}", category=ticketcat,
@@ -139,7 +142,31 @@ class reportsystemcmd(commands.Cog):
         except Exception as e:
             print(e)
 
+    @app_commands.checks.has_permissions(manage_channels=True)
+    @app_commands.command(name="setreportcategory", description="Command to set your server's report category.")
+    async def reportcategory(self, interaction: discord.Interaction, category: discord.CategoryChannel):
+        try:
+            await dbset(interaction.guild.id, self.bot.user.name, "categoryid", category.id)
+            await interaction.response.send_message(
+                f"Your report category has been set to {discord.utils.get(interaction.guild.categories, id=category.id)}.",
+                ephemeral=True)
+        except Exception as e:
+            print(e)
+            await interaction.response.send_message(content=f"""Something went wrong.""", ephemeral=True)
+
+    @app_commands.checks.has_permissions(manage_channels=True)
+    @app_commands.command(name="resetreportcategory", description="Command to reset your server's reportcategory.")
+    async def resetreportcategory(self, interaction: discord.Interaction):
+        try:
+            await dbset(interaction.guild.id, self.bot.user.name, "categoryid", 0)
+            await interaction.response.send_message(f"Report Category config has been reset.", ephemeral=True)
+        except Exception as e:
+            print(e)
+            await interaction.response.send_message(content=f"""Something went wrong.""", ephemeral=True)
+
     @report.error
+    @reportcategory.error
+    @resetreportcategory.error
     async def onerror(self, interaction: discord.Interaction, error: app_commands.MissingPermissions):
         await interaction.response.send_message(content=error,
                                                 ephemeral=True)
