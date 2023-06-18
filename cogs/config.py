921a1d32a56177208346540c6312e05df8097c7f
evanpurl <evan@purlcomputers.com>
2023-06-18 16:11:17 -0600

    Testing

--- cogs/config.py
+++ cogs/config.py
@@ -22,21 +22,21 @@ class Select(discord.ui.Select):
                 if ison[0] == 1:
                     await dbset(interaction.guild.id, interaction.client.user.name, "ismsglogon", 0)
                     await interaction.response.send_message(content=f"Message log **Disabled**", ephemeral=True)
-                    await interaction.followup.edit_message(view=SelectView())
+                    await interaction.response.edit_message(view=SelectView())
                 else:
                     await dbset(interaction.guild.id, interaction.client.user.name, "ismsglogon", 1)
                     await interaction.response.send_message(content=f"Message log **Enabled**", ephemeral=True)
-                    await interaction.followup.edit_message(view=SelectView())
+                    await interaction.response.edit_message(view=SelectView())
             if self.values[0] == "Report Transcript":
                 ison = await dbget(interaction.guild.id, interaction.client.user.name, "isreporttranscripton")
                 if ison[0] == 1:
                     await dbset(interaction.guild.id, interaction.client.user.name, "isreporttranscripton", 0)
                     await interaction.response.send_message(content=f"Report Transcript **Disabled**", ephemeral=True)
-                    await interaction.followup.edit_message(view=SelectView())
+                    await interaction.response.edit_message(view=SelectView())
                 else:
                     await dbset(interaction.guild.id, interaction.client.user.name, "isreporttranscripton", 1)
                     await interaction.response.send_message(content=f"Report Transcript **Enabled**", ephemeral=True)
-                    await interaction.followup.edit_message(view=SelectView())
+                    await interaction.response.edit_message(view=SelectView())
         except Exception as e:
             print(e)
             await interaction.response.send_message(content=f"""Something went wrong.""", ephemeral=True)
