3264a539107a93e28817d1e1f4137dd92d39162c
evanpurl <evan@purlcomputers.com>
2023-06-20 22:01:43 -0600

    Added member count commmand.

--- cogs/misc.py
+++ cogs/misc.py
@@ -15,6 +15,10 @@ class misccommands(commands.Cog):
 Discord: https://discord.gg/g8UbZ95QZh""",
             ephemeral=True)
 
+    @app_commands.command(name="member_count", description="Gets member count on the current server.")
+    async def mcount(self, interaction: discord.Interaction):
+        await interaction.response.send_message(content=f"Current member count in {interaction.guild.name}: {len([m for m in interaction.guild.members if not m.bot])}", ephemeral=True)
+
 
 async def setup(bot):
     await bot.add_cog(misccommands(bot))
