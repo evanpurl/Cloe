036f14b080e41f04dccad5a2d85622c0dc7cf78d
evanpurl <evan@purlcomputers.com>
2023-08-20 13:28:22 -0600

    Added install requirements section.

--- main.py
+++ main.py
@@ -3,6 +3,15 @@ import os
 import subprocess
 import sys
 
+
+def install_requirements():
+    subprocess.check_call([sys.executable, '-m', 'pip', 'install', '--upgrade', 'pip'])
+    if os.path.exists('requirements.txt'):
+        subprocess.check_call([sys.executable, '-m', 'pip', 'install', '-r', 'requirements.txt'])
+
+
+install_requirements()
+
 import discord
 from util.load_extensions import load_extensions  # Our code
 from discord.ext import commands
@@ -16,15 +25,8 @@ intents.members = True
 client = commands.Bot(command_prefix="$", intents=intents)
 
 
-async def install_requirements():
-    subprocess.check_call([sys.executable, '-m', 'pip', 'install', '--upgrade', 'pip'])
-    if os.path.exists('requirements.txt'):
-        subprocess.check_call([sys.executable, '-m', 'pip', 'install', '-r', 'requirements.txt'])
-
-
 # Main function to load extensions and then load bot.
 async def main():
-    await install_requirements()
     async with client:
         try:
             token = await gettoken("c1o3")
