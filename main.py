1e64cf5169963d1b855ef77c6fb9f3481ad9be25
evanpurl <evan@purlcomputers.com>
2023-09-25 21:09:53 -0600

    Added load_env to main.py, added python-dotenv to requirements.txt

--- main.py
+++ main.py
@@ -15,12 +15,15 @@ install_requirements()
 import discord
 from util.load_extensions import load_extensions  # Our code
 from discord.ext import commands
+from dotenv import load_dotenv
 
 intents = discord.Intents.default()
 intents.message_content = True
 intents.members = True
 
 client = commands.Bot(command_prefix="$", intents=intents)
+
+load_dotenv()
 
 
 # Main function to load extensions and then load bot.
