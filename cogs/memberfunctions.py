46ecb6359f85ef263ff5e7718b11ab55a2fb850b
evanpurl <evan@purlcomputers.com>
2023-09-25 20:42:13 -0600

    redid memberfunctions.py

--- cogs/memberfunctions.py
+++ cogs/memberfunctions.py
@@ -1,8 +1,9 @@
 import datetime
 
 import discord
+from discord import app_commands
 from discord.ext import commands
-from retiredcogs.dbsetget import dbget
+from util.sqlitefunctions import getconfig, create_db
 
 "needs welcomechannelid in db"
 
@@ -31,26 +32,25 @@ class memberfunctions(commands.Cog):
     @commands.Cog.listener()
     async def on_member_join(self, member):
         try:
-            wchannel = await dbget(member.guild.id, self.bot.user.name, "welcomechannelid")
-            channel = discord.utils.get(member.guild.channels, id=wchannel[0])
+            conn = await create_db(f"storage/{member.guild.id}/configuration.db")
+            wchannel = await getconfig(conn, "welcomechannelid")
+            channel = discord.utils.get(member.guild.channels, id=wchannel)
             if channel:
                 await channel.send(embed=userembed(member, member.guild))
-            roleid = await dbget(member.guild.id, self.bot.user.name, "defaultroleid")
-            role = discord.utils.get(member.guild.roles, id=roleid[0])
+            roleid = await getconfig(conn, "defaultroleid")
+            role = discord.utils.get(member.guild.roles, id=roleid)
+            conn.close()
             if role:
                 await member.add_roles(role)
         except Exception as e:
             print(e)
-            wchannel = await dbget(member.guild.id, self.bot.user.name, "welcomechannelid")
-            channel = discord.utils.get(member.guild.channels, id=wchannel[0])
-            if channel:
-                await channel.send(
-                    content=f"""Unable to set your role, make sure my role is higher than the role you're trying to add!""")
 
     @commands.Cog.listener()
     async def on_member_remove(self, member):
-        wchannel = await dbget(member.guild.id, self.bot.user.name, "goodbyechannelid")
-        channel = discord.utils.get(member.guild.channels, id=wchannel[0])
+        conn = await create_db(f"storage/{member.guild.id}/configuration.db")
+        wchannel = await getconfig(conn, "goodbyechannelid")
+        conn.close()
+        channel = discord.utils.get(member.guild.channels, id=wchannel)
         if channel:
             await channel.send(embed=userembedbye(member))
 
